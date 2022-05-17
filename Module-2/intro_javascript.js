9046a6ce781f68c2ea610f6aeca1798b59ad4835
Avinash-ray <rayavinash616@gmail.com>
2022-05-17 10:24:46 +0530

    intro to javascript

---
+++ Module-2/intro_javascript.js
@@ -0,0 +1,25 @@
+// printing in js
+console.log("welcome to learning javascript")
+
+// variable declaration in js
+// we dont need to tell type of data in js just write keywords let, var, const.
+let a= 10;
+console.log(a);
+let char= 'a'
+console.log(char);
+let string= "hello there";
+console.log(string)
+let bool= true;
+console.log(bool)
+
+// for loop in js
+for(let i=0; i<=5; i++){
+    console.log(i)
+}
+
+// while loop in js
+let i=0
+while(i<=3){
+    console.log(i)
+    i++
+}
