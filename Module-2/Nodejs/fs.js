c26b2ef348136337835bd20f33f48cb1ad4b79b7
Avinash-ray <rayavinash616@gmail.com>
2022-05-19 11:19:49 +0530

    nodejs fs CRUD

---
+++ Module-2/Nodejs/fs.js
@@ -0,0 +1,22 @@
+let fs= require('fs')
+let path= require('path')
+console.log(fs)
+
+// C R U D -- create read update delete
+
+// create and write in a file---writeFileSync
+let filepath= path.join(__dirname,'createfile.txt')
+fs.writeFileSync(filepath,'thanks for creating me')
+// if file is already created then it will override the content
+fs.writeFileSync(filepath,"override the content")
+
+// read a file---readFileSync
+let content= fs.readFileSync(filepath,"utf-8")
+console.log(content)
+
+// update a file----appendFileSync
+fs.appendFileSync(filepath,"\nupdate me without overriding")
+console.log(fs.readFileSync(filepath,"utf-8"))
+
+// delete a file---unlinkSync
+// fs.unlinkSync(filepath)
