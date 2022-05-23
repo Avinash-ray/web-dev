3159310fa5b534f5e896f4a380df1f91e716b1c1
Avinash-ray <rayavinash616@gmail.com>
2022-05-23 12:29:38 +0530

    q3 done

--- Module-2/Nodejs/questions/q3.js
+++ Module-2/Nodejs/questions/q3.js
@@ -1,3 +1,16 @@
 //Q. copy index.html file from module1 to a new folder inside module2 having name html.
 
+let path= require('path')
+let fs= require('fs')
 
+let newfolder= path.join(__dirname,"..","..","html q3")
+// console.log(newfolder)
+if(!fs.existsSync(newfolder))
+fs.mkdirSync(newfolder)
+
+let sourcepath= path.join(__dirname,'..','..','..','Module-1','index.html')
+console.log(sourcepath)
+
+let destinationpath= path.join(newfolder,'index.html')
+
+fs.copyFileSync(sourcepath,destinationpath)
