3a820aeeb00c4a81d86b220afd3e0d529517cdb0
Avinash-ray <rayavinash616@gmail.com>
2022-05-24 12:47:22 +0530

    give function name implemented

--- Module-2/Project/fileOrganizer.js
+++ Module-2/Project/fileOrganizer.js
@@ -1,13 +1,42 @@
 let fs= require('fs')
+let path= require('path')
 let folderpath= process.argv[2]
 // console.log(folderpath)
 let folderexists= fs.existsSync(folderpath)
 
+let extensions= {
+    Audio: ['.mp3'],
+    Video: ['.mp4','.mkv'],
+    Document: ['.doc','.xlsx','.pdf','.txt'],
+    Image: ['.jpeg','.jpg','.png','.gif'],
+    Software: ['.exe']
+}
+
 if(folderexists){
-    console.log("valid path")
+    // console.log("valid path")
     let files= fs.readdirSync(folderpath)
     console.log(files)
+    for(let i=0; i< files.length; i++){
+        let extName= path.extname(files[i])
+        // console.log(extName)
+        let foldername= giveFolderName(extName);
+        console.log("ext----", extName," foldername--",foldername)
+    }
 }
 else{
     console.log("please enter a valid path")
+}
+
+
+function giveFolderName(ext){
+    for(let key in extensions){
+    let extarr= extensions[key]
+        for(let i=0; i< extarr.length; i++){
+            if(extarr[i] == ext){
+                return key
+            }
+        }
+
+    }
+    return 'others'
 }
