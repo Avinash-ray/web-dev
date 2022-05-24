d9dc8d31f2baa220ed8fdf34023fd89ca51df53b
Avinash-ray <rayavinash616@gmail.com>
2022-05-24 11:31:48 +0530

    read dir

--- Module-2/Project/fileOrganizer.js
+++ Module-2/Project/fileOrganizer.js
@@ -5,6 +5,8 @@ let folderexists= fs.existsSync(folderpath)
 
 if(folderexists){
     console.log("valid path")
+    let files= fs.readdirSync(folderpath)
+    console.log(files)
 }
 else{
     console.log("please enter a valid path")
