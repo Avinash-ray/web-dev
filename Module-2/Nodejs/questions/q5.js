b6d273e8c8f872d2efb6adfb0d087c1275c4d647
Avinash-ray <rayavinash616@gmail.com>
2022-05-24 10:14:36 +0530

    q5 done

--- Module-2/Nodejs/questions/q5.js
+++ Module-2/Nodejs/questions/q5.js
@@ -1,2 +1,10 @@
 //move a file
 
+let fs= require('fs')
+let path= require('path')
+
+let sourcepath= path.join(__dirname,'q5.txt')
+let destinationpath= path.join(__dirname,'q5 folder','q5.txt')
+
+fs.copyFileSync(sourcepath,destinationpath)
+fs.unlinkSync(sourcepath)
