93c7329e2478c3cbeb3a4df72e15974751668912
Avinash-ray <rayavinash616@gmail.com>
2022-05-27 20:12:29 +0530

    GEC questions

--- Module-2/interview/Executioncontext.js
+++ Module-2/interview/Executioncontext.js
@@ -1,14 +1,28 @@
-console.log(a)
-var a= 10
-console.log(a)
+// q1
+// console.log(a)
+// var a= 10
+// console.log(a)
 
-function fun(){
-    var b= 20
-    return b
+// function fun(){
+//     var b= 20
+//     return b
+// }
+
+// console.log(c)
+// var c= 20
+
+// var d= fun()
+// console.log(d)
+
+// q2
+// var fun= function(){
+//     console.log("Hello world")
+// }
+// fun()
+
+// q3
+fun();
+var fun= function(){
+    console.log("hello world")
 }
-
-console.log(c)
-var c= 20
-
-var d= fun()
-console.log(d)
+fun();
