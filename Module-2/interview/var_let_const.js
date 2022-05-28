45c3631d95383bd7fd5e72cc4c07f47fbc32693a
Avinash-ray <rayavinash616@gmail.com>
2022-05-28 11:09:03 +0530

    let type

--- Module-2/interview/var_let_const.js
+++ Module-2/interview/var_let_const.js
@@ -1,9 +1,18 @@
 // var
 
-console.log(a)
-var a= 10
+// console.log(a)
+// var a= 10
+// console.log(a)
+// a= 20
+// console.log(a)
+// var a= 50
+// console.log(a)
+
+// let
+
+// console.log(a) // cant access a before initialization // TDZ
+let a= 10;
 console.log(a)
 a= 20
 console.log(a)
-var a= 50
-console.log(a)
+let a =50  // cannot redeclare
