25e1acad4a332d79f57c5b02642544883328ba9f
Avinash-ray <rayavinash616@gmail.com>
2022-05-28 11:12:12 +0530

    const type

--- Module-2/interview/var_let_const.js
+++ Module-2/interview/var_let_const.js
@@ -1,6 +1,6 @@
 // var
 
-// console.log(a)
+// console.log(a)   // Hoisting
 // var a= 10
 // console.log(a)
 // a= 20
@@ -11,8 +11,18 @@
 // let
 
 // console.log(a) // cant access a before initialization // TDZ
-let a= 10;
+// let a= 10;
+// console.log(a)
+// a= 20
+// console.log(a)
+// let a =50  // cannot redeclare
+
+
+// const
+
+// console.log(a)  // TDZ
+const a= 10
 console.log(a)
-a= 20
-console.log(a)
-let a =50  // cannot redeclare
+// a= 20  // cannot reinitialize
+// const a= 20  // cannot redeclare
+
