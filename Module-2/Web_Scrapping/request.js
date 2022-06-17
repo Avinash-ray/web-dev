5b040973c926a7aa42773f63824665da89ce13bd
Avinash-ray <rayavinash616@gmail.com>
2022-06-17 08:54:21 +0530

    request and jsdom for webscrapping

--- Module-2/Web_Scrapping/request.js
+++ Module-2/Web_Scrapping/request.js
@@ -1,24 +1,11 @@
 const request= require('request')
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 
-const link= "https://sports.ndtv.com/ipl-2022"
 
-request(link, function (error, response, html) {
+request("https://www.google.com/", function (error, response, body) {
     if(error)
   console.error('error:', error); // Print the error if one occurred
   
   else{
-  // console.log('body:', html); // Print the HTML for the Google homepage.
-  const dom= new JSDOM(html)
-  const document= dom.window.document
-
-  let teamsName= document.querySelectorAll(".scr_tm-wrp .scr_tm-nm")
-  console.log(teamsName[0].textContent)
-  console.log(teamsName[1].textContent)
-  console.log(teamsName[2].textContent)
-  console.log(teamsName[3].textContent)
-  console.log(teamsName[4].textContent)
-  console.log(teamsName[5].textContent)
+  console.log('body:', body); // Print the HTML for the Google homepage.
   }
 });
