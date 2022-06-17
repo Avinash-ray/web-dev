35c9c2cb0136ae050bc9e8507f30329ff99764a6
Avinash-ray <rayavinash616@gmail.com>
2022-06-17 08:44:59 +0530

    webscrapping from ipl webpage

--- Module-2/Web_Scrapping/request.js
+++ Module-2/Web_Scrapping/request.js
@@ -1,10 +1,24 @@
 const request= require('request')
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-request('http://www.google.com', function (error, response, body) {
+const link= "https://sports.ndtv.com/ipl-2022"
+
+request(link, function (error, response, html) {
     if(error)
   console.error('error:', error); // Print the error if one occurred
   
   else{
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  // console.log('body:', html); // Print the HTML for the Google homepage.
+  const dom= new JSDOM(html)
+  const document= dom.window.document
+
+  let teamsName= document.querySelectorAll(".scr_tm-wrp .scr_tm-nm")
+  console.log(teamsName[0].textContent)
+  console.log(teamsName[1].textContent)
+  console.log(teamsName[2].textContent)
+  console.log(teamsName[3].textContent)
+  console.log(teamsName[4].textContent)
+  console.log(teamsName[5].textContent)
   }
 });
